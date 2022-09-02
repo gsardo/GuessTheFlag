f963521479852d5bd01e5fa26ba88a8c136b4f2a
Giuseppe Sardo <giuseppe.sardo@clearscore.com>
2022-09-02 15:36:42 +1000

    GuessTheFlag first commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,130 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var questionCounter = 1
+    @State private var showingScore = false
+    @State private var showingResults = false
+    @State private var scoreTitle = ""
+    @State private var scoreCount = 0
+    
+    @State private var countries = allCountries.shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    struct FlagImage: View {
+        var name: String
+        
+        var body: some View {
+            Image(name)
+                .renderingMode(.original)
+                .clipShape(Capsule())
+                .shadow(radius: 5)
+        }
+    }
+    
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            
+            LinearGradient(gradient: Gradient(colors: [.blue, .orange]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the flag")
+                    .font(.largeTitle.bold())
+                
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundColor(.black)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .foregroundColor(.black)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button{
+                            flagTapped(number)
+                        } label: {
+                 FlagImage(name: countries[number])
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(scoreCount)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
             .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(scoreCount)")
+        }
+        .alert("Game over!", isPresented: $showingResults) {
+            Button("Start again", action: newGame)
+        } message: {
+            Text("Your final score was \(scoreCount)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            scoreCount += 1
+            
+        } else {
+            let needsThe = ["UK", "US"]
+            let theirAnswer = countries[number]
+            
+            if needsThe.contains(theirAnswer) {
+                scoreTitle = "Wrong, that's the flag of the \(theirAnswer)"
+            } else {
+                scoreTitle = "Wrong, that's the flag of \(theirAnswer)"
+            }
+            
+            if scoreCount > 0 {
+                scoreCount -= 1
+            }
+        }
+        
+        if questionCounter == 8 {
+            showingResults = true
+        } else {
+            showingScore = true
+        }
+        
+    }
+    
+    func askQuestion() {
+        countries.remove(at: correctAnswer)
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        questionCounter += 1
+    }
+    
+    func newGame() {
+        questionCounter = 0
+        scoreCount = 0
+        countries = Self.allCountries
+        askQuestion()
     }
 }
 
